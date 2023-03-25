1bfa71bb302256a256ad56816fd14d9985769aec
David Yang <davidyang@Davids-MacBook-Pro.local>
2023-03-25 15:58:17 -0700

    add dropdown menu

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,20 +1,36 @@
 import { View, Text, Button, FlatList, TextInput, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 import DiaryItem from '../DiaryItem';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Home({ navigation, route }) {
   const [diaries, setDiaries] = useState([{author:'lesly',species:'bamboo',date:'2023-03-24',location:'Downtown Vancouver',likes:4},{author:'boris',species:'rose',date:'2023-03-21',location:'Surrey',likes:16}]);
   const [search, setSearch] = useState("");
-  const [sort, setSort] = useState("");
+  const [sort, setSort] = useState("recommand");
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    {label: 'Sorted by: Recommand', value: 'recommand'},
+    {label: 'By Location', value: 'location'},
+    {label: 'By Species', value: 'species'}
+  ]);
   const [recommend, setRecommend] = useState(route.params.recommend);
 
   return (
     <SafeAreaView>
       {recommend && 
-      <View>
+      <View style={{ zIndex: 100 }}>
         <TextInput placeholder='search a plant' value={search} onChangeText={setSearch} />
         <Button title='Search' />
-        <Button title='Sort by' />
+        <View>
+        <DropDownPicker
+          open={open}
+          value={sort}
+          items={items}
+          setOpen={setOpen}
+          setValue={setSort}
+          setItems={setItems}
+        />
+        </View>
       </View>}
       <View>
         <FlatList
