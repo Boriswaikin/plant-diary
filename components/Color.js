199a291e1897f855e4c6c80b77fb8fe4af2e417c
Boriswaikin <plmnboris@gmail.com>
2023-03-25 23:46:26 -0700

    update color component

---
+++ components/Color.js
@@ -0,0 +1,10 @@
+/**
+ * //This is a helper file used to expose common color value
+ */
+const Color = {
+    transparent: "#B1AFFF",
+    rippleEffect: "#B1AFFF",
+    buttonEffect: "rgba(210, 230, 255, 0.1)",
+  };
+  export default Color;
+  
