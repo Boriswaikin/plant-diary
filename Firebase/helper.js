3937810904896ac57788ad1c96ba6b205d2731fe
lesley <chen.siqi2@northeastern.edu>
2023-03-25 22:51:14 -0700

    add create diary in helper file

---
+++ Firebase/helper.js
@@ -0,0 +1,36 @@
+import { async } from "@firebase/util";
+import {
+	collection,
+	addDoc,
+	deleteDoc,
+	doc,
+	updateDoc,
+} from "firebase/firestore";
+import { firestore } from "./firebase-setup";
+// Add a new document with a generated id.
+export async function createDiary(diary) {
+	console.log("call create diary function");
+	const docRef = await addDoc(collection(firestore, "diary"), {
+		photos: diary.photos,
+		description: diary.description,
+		species: diary.species,
+		location: diary.location,
+		date: diary.date,
+		userId: diary.userId,
+	});
+	console.log("Diary written with ID: ", docRef.id);
+}
+export async function deleteDiary(id) {
+	try {
+		await deleteDoc(doc(firestore, "diary", id));
+		console.log("Document deleted with ID: ", id);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+export async function editDiary(id, updateField) {
+	const docRef = doc(firestore, "diary", id);
+	await updateDoc(docRef, updateField);
+	console.log("Document updated with ID: ", id);
+}
