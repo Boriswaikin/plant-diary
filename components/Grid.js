c28534fa7edfcee2879b3b4021f5541ec920848b
Boriswaikin <plmnboris@gmail.com>
2023-03-25 23:34:12 -0700

    update Grid styles

--- components/Grid.js
+++ components/Grid.js
@@ -65,5 +65,23 @@ const Grid = (props) => {
   )
 }
 
+const styles = (props) => StyleSheet.create({
+    container: {
+        width:props.width,
+        alignSelf:props.alignSelf,
+        marginTop:props.marginTop,
+        marginRight:props.marginRight,
+      },
+
+    item: {
+        flex: 1,
+        alignItems:"flex-start",
+        padding: 5,
+      },
+      imageIcon:{
+        width:props.width/props.numColumns,
+        height:props.width/props.numColumns,
+      }
+  });
 
   export default Grid;
