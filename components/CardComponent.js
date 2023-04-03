afed1ca458a3975f509b1d03eb24684fcf8c69f2
Boriswaikin <plmnboris@gmail.com>
2023-04-03 10:57:03 -0700

    update to predefine some parameter

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import React from "react";
-
+import Color from "./Color";
 /**
  * This is the card component that will be used
  * @param props.flexDirection : the flexDirection of the card
@@ -10,10 +10,7 @@ import React from "react";
  * @param props.width : the width of the card
  * @param props.height : the height of the card
  * @param props.radius : the border radius of the card
- * @param props.marginTop : the margin Top of the card
- * @param props.marginLeft : the margin Left of the card
  * @param props.marginBottom : the margin Bottom of the card
- * @param props.marginRight : the margin Right of the card
  * @returns the card configuration
  */
 const CardComponent = (props) => {
@@ -24,15 +21,12 @@ const styles = (props) =>
     body: {
       flexDirection: props.flexDirection,
       justifyContent: props.justifyContent,
-      alignItems: props.alignItems,
-      backgroundColor: props.color,
       width: props.width,
       height: props.height,
-      borderRadius: props.radius,
-      marginTop: props.marginTop,
-      marginLeft: props.marginLeft,
-      marginBottom: props.marginBottom,
-      marginRight: props.marginRight,
+      alignItems: props.alignItems,
+      backgroundColor: Color.contentColor,
+      borderRadius: 20,
+      marginBottom: 15,
     },
   });
 
