c5d09abc4fee414d54f3774742c18e37103711d2
SiqiChen2 <chen.siqi2@northeastern.edu>
2023-04-16 09:43:20 -0700

    clear previous useEffect for sort and recommend function

--- Screens/Home.js
+++ Screens/Home.js
@@ -79,6 +79,7 @@ export default function Home({ navigation, route }) {
 							);
 							return distanceFromA - distanceFromB;
 						});
+						//setLocation(null);
 					}
 					setDiaries(newdiaries);
 				}
